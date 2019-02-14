44a6da8a423edeee1cfde1922823b3dd69b272cd
KueiChinHuang <200397841@student.georgianc.on.ca>
2019-02-14 00:29:08 -0500

    v0.6

--- index.php
+++ index.php
@@ -55,45 +55,35 @@ if (!empty($_GET['id']) && (is_numeric($_GET['id']))) {
         <div class="form-group">
           <input type="text" name="skill" class="form-control" placeholder="Assess your climbing skill (1 - 10)" value="<?php echo $skill ?>">
         </div>
+
         <div class="form-group">
-         <!--  <select name="experience"  onchange="this.style.color='#555'">
-            <option disabled selected value>How long have you been climbing?</option>
-            <option value="Never climb before">Never climb before</option>
-            <option value="Under 3 months">Under 3 months</option>
-            <option value="3-6 months">3-6 months</option>
-            <option value="6 months - 1 years">6 months - 1 years</option>
-            <option value="1 year - 2 years">1 year - 2 years</option>
-            <option value="2 - 5 years">2 - 5 years</option>
-            <option value="5 - 10 years">5 - 10 years</option>
-            <option value="Over 10 years">Over 10 years</option>
-          </select> -->
-
-<select name="experience"  onchange="this.style.color='#555'">
-<?php
-$theArray = array(
-  "How long have you been climbing?",
-  "Never climb before",
-  "Under 3 months",
-  "3 - 6 months",
-  "6 months - 1 years",
-  "1 year - 2 years",
-  "2 - 5 years",
-  "5 - 10 years",
-  "Over 10 years"
-);
-
-foreach ($theArray as $value) {
-    if ($value == $experience) {
-        echo('<option selected="selected" value="'.$value.'">'.$value.'</option>');
-    } else {
-        echo('<option value="'.$value.'">'.$value.'</option>');
-    }
-}
-?>
-</select>
-
+          <select name="experience"  onchange="this.style.color='#555'">
+          <?php
+            $theArray = array(
+              "How long have you been climbing?",
+              "Never climb before",
+              "Under 3 months",
+              "3 - 6 months",
+              "6 months - 1 years",
+              "1 year - 2 years",
+              "2 - 5 years",
+              "5 - 10 years",
+              "Over 10 years"
+            );
+            foreach ($theArray as $value) {
+              if ($value == "How long have you been climbing?") {
+                echo('<option disabled selected value>'.$value.'</option>');
+              } else if ($value == $experience) {
+                echo('<option selected="selected" value="'.$value.'">'.$value.'</option>');
+              } else {
+                echo('<option value="'.$value.'">'.$value.'</option>');
+              }
+            }
+          ?>
+          </select>
         </div>
-        <!-- need t add this hidden input so we know which record we are deleting ! -->
+
+        <!-- need to add this hidden input so we know which record we are deleting ! -->
           <input type="hidden" value="<?php echo $id ?>" name="id">
           <input type="submit" name="submit" value="submit" class="btn btn-primary">
       </form>
